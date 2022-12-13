84da616af7ecd30f563405dd544f3f939f48a0c1
ManuelSarmientoSauco <manu.sarmientovk@gmail.com>
2022-12-13 23:13:34 +0100

    add loggers

--- Practice3/WeatherService/src/main/java/es/mastercloud/weather/WeatherGrpcService.java
+++ Practice3/WeatherService/src/main/java/es/mastercloud/weather/WeatherGrpcService.java
@@ -2,22 +2,26 @@ package es.mastercloud.weather;
 
 import io.grpc.stub.StreamObserver;
 import io.quarkus.grpc.GrpcService;
-import io.smallrye.mutiny.Uni;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
-import java.util.Random;
-import java.util.logging.Logger;
+
 
 @GrpcService
 public class WeatherGrpcService extends WeatherServiceGrpc.WeatherServiceImplBase {
+    private final Logger logger = LoggerFactory.getLogger(WeatherGrpcService.class);
     private static final String VOWELS = "aeiou";
-
     @Override
     public void getWeather(GetWeatherRequest request, StreamObserver<Weather> responseObserver) {
-        responseObserver.onNext(Weather.newBuilder().setCity(request.getCity()).setWeather(
+        logger.debug("getWeather request: "+ request.getCity());
+
+        Weather response = Weather.newBuilder().setCity(request.getCity()).setWeather(
                 VOWELS.contains(request.getCity().toLowerCase().substring(0,1))?"Rainy":"Sunny"
-        ).build());
+        ).build();
+
+        logger.debug("getWeather response: "+ response.getWeather());
+
+        responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 }
